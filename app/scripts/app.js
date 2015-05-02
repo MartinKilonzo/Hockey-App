03ab60ecebace327ada2d9c3c861bc034ce413de
MartinKilonzo <martinkilonzo2@gmail.com>
2015-05-02 19:42:40 -0400

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('HockeyApp', ['ngAnimate', 'ngCookies', 'ngRoute'])
+
+  .constant('version', 'v0.0.1')
+
+  .config(function($locationProvider, $routeProvider) {
+
+    $locationProvider.html5Mode(false);
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home.html'
+      })
+      .when('/features', {
+        templateUrl: 'views/features.html'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+  });
