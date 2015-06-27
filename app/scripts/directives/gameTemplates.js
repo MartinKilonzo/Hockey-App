a9810e5ecd89517459d8926cb8fe7d8ad7e9993e
MartinKilonzo <martinkilonzo2@gmail.com>
2015-06-26 20:36:11 -0400

    Encapsulated game components into partials

---
+++ app/scripts/directives/gameTemplates.js
@@ -0,0 +1,45 @@
+'use-strict';
+
+angular.module('HockeyApp')
+
+	.directive('gameButtons', function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'views/partials/game/game-buttons.html'
+		};
+	})
+
+	.directive('gameToggles', function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'views/partials/game/game-toggles.html'
+		};
+	})
+
+	.directive('gameBoard', function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'views/partials/game/game-board.html'
+		};
+	})
+
+	.directive('gameHistory', function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'views/partials/game/game-history.html'
+		};
+	})
+
+	.directive('gamePlayer', function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'views/partials/game/game-player.html'
+		};
+	})
+
+	.directive('gameLineup', function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'views/partials/game/game-lineup.html'
+		};
+	});
---
+++ app/scripts/directives/undoDirective.js
@@ -0,0 +1,57 @@
+'use-strict';
+
+angular.module('HockeyApp')
+
+	.directive('undoSupport', function () {
+		return {
+			controller: function($scope) {
+				var execuStack = [];
+				var execuPointer = 0;
+
+				this.push = function(action) {
+					execuStack[execuPointer] = action;
+					execuPointer++;
+
+					//Empty the older actions in the stack ie. the redo portion of the stack
+					execuStack.length = execuPointer;					
+				};
+
+				$scope.undo = function () {
+					if ($scope.undoable) {
+						execuPointer--;
+						var undoAction = excuStack[execuPointer];
+						undoAction.unExecute();
+						$log.info('undone');
+					}
+				};
+
+				$scope.redo = function () {
+					if ($scope.redoable) {
+						var redoAction = execuStack[execuPointer];
+						redoAction.exectute();
+						execuPointer++;
+						$log.info('redone');
+					}
+				};
+
+				$scope.undoable = false;
+				$scope.redoable = false;
+
+				// Watch to see if undoing is possible
+				$scope.$watch( function () { 
+					return execuStack.length > 0 && execuPointer >= 0;
+				}, function (undoable) { 
+					$scope.undoable = undoable
+				});
+
+				// Watch to see if redoing is possible
+				$scope.$watch( function () {
+					return execuStack.length > 0 && execuPointer <= execuStack.length - 1;
+				}, function (redoable) {
+					$scope.redoable = redoable;
+				});
+			}
+		};
+	})
+
+	.directive('undoAble', )
