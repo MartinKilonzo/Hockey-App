49d9207d138972f491f1ff4b5db19e6ea1107cd1
MartinKilonzo <martinkilonzo2@gmail.com>
2015-07-12 22:25:36 -0400

    Fixed bug where hyperlinks were being blocked

--- app/scripts/directives/selectableDirective.js
+++ app/scripts/directives/selectableDirective.js
@@ -6,19 +6,27 @@ angular.module('HockeyApp')
 		return { 
 			restrict: 'A',
 			link: function (scope, element, attrs) {
-				element.data('selectable', true); // Bind the value to the element
+					element.data('selectable', true); // Bind the value to the element
 
-				angular.element($document[0].body).click( function (event) {
-					var selectable = angular.element(event.target).inheritedData('selectable');
-					event.stopImmediatePropagation(); // Prevents firing from each 'selectable' DOM element
-					
-					// On offclick, reset the seleciton
-					if (!selectable) {
-						console.log(scope.positionSelection);
-						scope.setSelectedPosition(undefined);
-						scope.$apply(); // Run digest loop
-					}
-				});
-			}
-		};
-	}]);
+					angular.element($document[0].body).click( function (event) {
+						var selectable = angular.element(event.target).inheritedData('selectable');
+
+						if (selectable) {
+							event.stopImmediatePropagation(); // Prevents firing from each 'selectable' DOM element
+						}
+
+						// On offclick, reset the seleciton
+						if (!selectable) {
+							var target = $(event.target);
+
+							// If the element is a link, stopping propagation breaks the link event
+							if (!target.is('a')) {
+								event.stopImmediatePropagation();
+							}
+							scope.setSelectedPosition(undefined);
+							scope.$apply(); // Run digest loop
+						}
+					});
+				}
+			};
+		}]);
