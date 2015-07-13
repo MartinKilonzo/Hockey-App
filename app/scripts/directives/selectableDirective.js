39b8ec8cd3e63ad3dc90cedea24147ff60c8fd94
MartinKilonzo <martinkilonzo2@gmail.com>
2015-07-12 21:34:28 -0400

    Created deselect-on-offclick for player selections

---
+++ app/scripts/directives/selectableDirective.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('HockeyApp')
+
+	.directive('selectable', ['$document', '$window', function ($document, $window) {
+		return { 
+			restrict: 'A',
+			link: function (scope, element, attrs) {
+				element.data('selectable', true); // Bind the value to the element
+
+				angular.element($document[0].body).click( function (event) {
+					var selectable = angular.element(event.target).inheritedData('selectable');
+					event.stopImmediatePropagation();
+					
+					// On offclick, reset the seleciton
+					if (!selectable) {
+						console.log(scope.positionSelection);
+						scope.setSelectedPosition(undefined);
+						scope.$apply();
+					}
+				});
+			}
+		};
+	}]);
