49af42f56af0484e9ba9dac1882eaf794b74fbfc
MartinKilonzo <martinkilonzo2@gmail.com>
2015-07-12 22:32:14 -0400

    Prevented selection reset on player selection

--- app/scripts/directives/selectableDirective.js
+++ app/scripts/directives/selectableDirective.js
@@ -2,31 +2,33 @@
 
 angular.module('HockeyApp')
 
-	.directive('selectable', ['$document', '$window', function ($document, $window) {
-		return { 
-			restrict: 'A',
-			link: function (scope, element, attrs) {
-					element.data('selectable', true); // Bind the value to the element
+.directive('selectable', ['$document', '$window', function ($document, $window) {
+	return { 
+		restrict: 'A',
+		link: function (scope, element, attrs) {
+			element.data('selectable', true); // Bind the value to the element
 
-					angular.element($document[0].body).click( function (event) {
-						var selectable = angular.element(event.target).inheritedData('selectable');
+			angular.element($document[0].body).click( function (event) {
+				var selectable = angular.element(event.target).inheritedData('selectable');
 
-						if (selectable) {
-							event.stopImmediatePropagation(); // Prevents firing from each 'selectable' DOM element
-						}
-
-						// On offclick, reset the seleciton
-						if (!selectable) {
-							var target = $(event.target);
-
-							// If the element is a link, stopping propagation breaks the link event
-							if (!target.is('a')) {
-								event.stopImmediatePropagation();
-							}
-							scope.setSelectedPosition(undefined);
-							scope.$apply(); // Run digest loop
-						}
-					});
+				if (selectable) {
+					event.stopImmediatePropagation(); // Prevents firing from each 'selectable' DOM element
 				}
-			};
-		}]);
+
+				// On offclick, reset the seleciton
+				if (!selectable) {
+					var target = $(event.target);
+
+					// If the element is a link, stopping propagation breaks the link event
+					// Likewise, if the element is a player to be swapped, do not reset the selection
+					if (!(target.is('a') || target.has('game-playerpool'))) {
+						event.stopImmediatePropagation();
+						scope.setSelectedPosition(undefined);
+						scope.$apply(); // Run digest loop
+					}
+					
+				}
+			});
+		}
+	};
+}]);
