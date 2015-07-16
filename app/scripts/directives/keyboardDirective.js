25334a713b9c5d7584c1227599f6af74338bf87c
MartinKilonzo <martinkilonzo2@gmail.com>
2015-07-15 23:45:44 -0400

    Fixed a bug that caused undo and redo to invalidate the action stack pointer

--- app/scripts/directives/keyboardDirective.js
+++ app/scripts/directives/keyboardDirective.js
@@ -5,45 +5,44 @@ angular.module('HockeyApp')
 	.directive('keyboardSupport', function () {
 		return {
 			restrict: 'A',
-			controller: 'gameController',
 			link: function (scope, element, attrs, ctrl) {
-				console.log(ctrl);
 				var page = element.closest('html');
-				console.log(page);
 				page.bind('keydown', function (event) {
 					
 					// CTRL + keys
 					if (event.ctrlKey) {
-						if (event.which === 90)
-							ctrl.undo();
+						if (event.which === 90){
+							scope.$apply(scope.undo());
+						}
 
-						if (event.which === 89)
-							ctrl.redo();
+						if (event.which === 89){
+							scope.$apply(scope.redo());
+						}
 					}
 					//TODO: Make these modifiable from settingsController
 					// q = +1 shots on
 					else if (event.which === 81) {
-						ctrl.addShotsOn();
+						scope.addShotsOn();
 					}
 					// a = -1 shots on
 					else if (event.which === 65) {
-						ctrl.subtShotsOn();
+						scope.subtShotsOn();
 					}
 					// w = +1 shots against
 					else if (event.which === 87) {
-						console.log('w');
+						scope.addSotsAgainst();
 					}
 					// s = -1 shots against
 					else if (event.which === 83) {
-						ctrl.subtShotsAgainst();
+						scope.subtShotsAgainst();
 					}
 					// e = +1 team goals
 					else if (event.which === 69) {
-						ctrl.addTeamGoal();
+						scope.addTeamGoal();
 					}
 					// d = =1 opponent goals
 					else if (event.which === 68) {
-						ctrl.addOpponentGoal();
+						scope.addOpponentGoal();
 					}
 
 				});
