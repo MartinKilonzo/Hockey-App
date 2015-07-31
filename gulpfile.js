643aa7a97b7c0519f21712f86fdd6927ae9f3e3b
MartinKilonzo <martinkilonzo2@gmail.com>
2015-07-31 04:49:02 -0400

    Added insertion and deletion communication for players through Roster between the database and the client

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,40 @@ var gulp = require('gulp');
 var config = require('ng-factory').use(gulp);
 var chalk = require('chalk');
 
-//
-// Aliases
+var express = require('express');
+var parser = require('body-parser');
+var cors = require('cors');
+var mongoose = require('mongoose');
 
-var startExpress = function () {
-	var express = require('express');
-	var app = express();
-	var port = 8999;
+var playerdbCtrl = require('./api/controllers/playerdb.js'); 
 
-	app.get('test', function (req, res) {
-		console.log('Get Success on port' + port)
-		});
-	app.post('api/gamedb', function (req, res) {
-		console.log('Posting' + this);
-		});
+//** EXPRESS **//
+var server = express();
+var serverPort = 8999;
+
+var configureServer = function () {
+	server.use(cors());
+	server.use(parser.urlencoded({ extended: false }));
+	server.use(parser.json());
+};
+
+
+var startExpress = function () {	
+	configureServer();
+	//mongoose.connect('mongodb://localhost:27017/gamedb');
+	mongoose.connect('mongodb://localhost:27017/app-test');
+
+	/* GET METHODS */
 	
-	app.listen(port, function () {
+	
+	/* POST METHODS */
+	server.post('/api/players', playerdbCtrl.create);
+
+	/* DELETE METHODS */
+	server.delete('/api/players/:resourceId', playerdbCtrl.delete);
+
+	/* SERVER */
+	server.listen(serverPort, function () {
 		var getTime = function () {
 			var date = new Date();
 			var time = [date.getHours(), date.getMinutes(), date.getSeconds()];
@@ -32,8 +50,8 @@ var startExpress = function () {
 
 			return time[0] + ':' + time[1] + ':' + time[2];
 		};
-		console.log('[' + chalk.gray(getTime()) + ']' + ' Database \'' + chalk.blue('Listening on port ' + port) + '\'...');
-		});
+		console.log('[' + chalk.gray(getTime()) + ']' + ' Database \'' + chalk.blue('Listening on port ' + serverPort) + '\'...');
+	});
 };
 
 var serve = gulp.series('ng:serve');
@@ -44,7 +62,11 @@ gulp.task('build', build);
 gulp.task('start', function () {
 	startExpress();
 	serve();
-	});
+});
+gulp.task('data', function () {
+gulp.watch('./api/**/*.js');
+	startExpress();
+});
 
 //
 // Hooks example
