fd45c115265ac0a2e757f73efc693c292e80d4c6
MartinKilonzo <martinkilonzo2@gmail.com>
2015-07-31 04:48:33 -0400

    Added insertion and deletion communication for players through Roster between the database and the client

---
+++ api/controllers/playerdb.js
@@ -0,0 +1,28 @@
+'use strict';
+var mongoose = require('mongoose');
+var playerModels = require('../models/playerModel.js')(mongoose);
+var Player = playerModels.player;
+
+module.exports.create = function (req, res) {
+	console.log('Creating...', req.body);
+	var newPlayer = new playerModels.Player({	firstName: req.body.firstName,
+										    lastName: req.body.lastName,
+										    playerNumber: req.body.playerNumber,
+										    position: req.body.position,
+										    games: req.body.games			});
+
+	newPlayer.save( function (err, result) {
+		res.json(result);
+	});
+};
+
+module.exports.delete = function (req, res) {
+	console.log('Deleting...', req.params);
+	var ObjectId = mongoose.Types.ObjectId;
+	var query = playerModels.Player.where({ _id: new ObjectId(req.params.resourceId) });
+
+	// Can use findOne() and Remove() seperately to store deleted data
+	query.findOneAndRemove( function (err, result) {
+		res.json(result);
+	});
+};
